622cdaba426bfeabc8189160f67bf0f94e24e5cf
Layhout-09 <tanglayhout@gmail.com>
2026-06-03 13:00:53 +0700

    Tesing 5

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () { return view('homepage')});
 Route::get('/signup', function () { return view('signup')});
+Route::get('/welcome', function () { return view('welcome')});
