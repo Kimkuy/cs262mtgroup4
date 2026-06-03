7651db73b961643ae7b9009890843eb79f2444f3
Ahtino <ltang1@paragoniu.edu.kh>
2026-06-03 12:54:19 +0700

    fourth testing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('homepage');
-});
+Route::get('/', function () { return view('homepage')});
+Route::get('/signup', function () { return view('signup')});
