933d8f2a16d13fdb495717017b3b46e6cc05122a
Layhout-09 <tanglayhout@gmail.com>
2026-06-05 01:43:04 +0700

    Home page is finished

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\PostController;
 Route::get('/', function () { return view('homepage');});
 Route::get('/signup', function () { return view('signup');});
 Route::get('/index', function () { return view('index');});
+Route::get('/thenews', function () { return view('thenews');});
 // Route::get('/layout', function () { return view('layout');});
 
 Route::post('/register', [UserController::class, 'register']);
