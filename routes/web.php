d3c97497ec36503bf42fdad30b20e184cf0a1198
Layhout-09 <tanglayhout@gmail.com>
2026-06-05 03:01:44 +0700

    edit minh ot tv lov edit tmey

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::get('/', function () { return view('index');});
 Route::get('/signup', function () { return view('signup');});
 Route::get('/homepage', function () { return view('homepage');});
 Route::get('/thenews', function () { return view('thenews');});
+Route::get('/index', function () { return view('index');});
 // Route::get('/layout', function () { return view('layout');});
 
 Route::post('/register', [UserController::class, 'register']);
